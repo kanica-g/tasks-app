4730ec30467cda782d98d5a83966cb3f1b750b74
kanicagoddard.kanica <kanicagoddard@gmail.com>
2025-07-21 23:04:55 +0100

    add server entrypoint and express app

---
+++ apps/server/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { tasksRouter } from './modules/tasks/task.routes';
+
+const app = express();
+
+// Apply middlewares
+app.use(express.json())
+
+// Apply endpoint routers
+app.use("/api/v1/tasks", tasksRouter)
+
+export { app };
