f5706f535697c052238c14c3de959b238c731e0f
kanicagoddard.kanica <kanicagoddard@gmail.com>
2025-07-22 01:07:09 +0100

    add patch endpoint, sort list

--- apps/server/src/modules/tasks/task.routes.ts
+++ apps/server/src/modules/tasks/task.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express"
 import { db } from "../../db";
 import { Task, tasksTable } from "../../db/schema";
-import { eq } from "drizzle-orm"
+import { eq, asc } from "drizzle-orm"
 import { ApiResponse } from "../../types";
-import { createTaskSchema, deleteTaskParamsSchema } from "./task.schemas";
+import { createTaskSchema, deleteTaskParamsSchema, patchTaskParamsSchema, patchTaskSchema } from "./task.schemas";
 
 const router: Router = Router();
 
 router.get("/", async (req, res) => {
     // Get tasks from DB
-    const allTasks = await db.select().from(tasksTable);
+    const allTasks = await db.select().from(tasksTable).orderBy(asc(tasksTable.createdAt));
 
     // Return result
     res.status(200).json({
@@ -43,6 +43,43 @@ router.post("/", async (req, res) => {
         data: inserted
     })
 })
+
+router.patch("/:id", async (req, res) => {
+    // Validate params
+    const parsedParams = patchTaskParamsSchema.safeParse(req.params);
+
+    if (!parsedParams.success) {
+        return res.status(400).json({
+            success: false,
+            message: parsedParams.error.message,
+        });
+    }
+
+    const { id } = parsedParams.data;
+
+    // Validate body
+    const parsedBody = patchTaskSchema.safeParse(req.body);
+
+    if (!parsedBody.success) {
+        return res.status(400).json({
+            success: false,
+            message: parsedBody.error.message,
+        });
+    }
+
+    const { done } = parsedBody.data;
+
+
+    // Patch task
+    const updated = await db
+        .update(tasksTable)
+        .set({ done })
+        .where(eq(tasksTable.id, id))
+        .returning();
+
+    // Return updated task
+    res.status(200).json({ success: true, data: updated });
+});
 
 router.delete("/:id", async (req, res) => {
     // Validate params
