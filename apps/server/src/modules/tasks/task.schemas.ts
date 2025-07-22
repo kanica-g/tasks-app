a05e399aa4c64c6e692a6591d577ae07c69eb40f
kanicagoddard.kanica <kanicagoddard@gmail.com>
2025-07-22 01:06:54 +0100

    add patch schemas

--- apps/server/src/modules/tasks/task.schemas.ts
+++ apps/server/src/modules/tasks/task.schemas.ts
@@ -4,6 +4,14 @@ export const createTaskSchema = z.object({
     title: z.string().min(1, "Title is required"),
 });
 
+export const patchTaskSchema = z.object({
+    done: z.boolean("Done is required"),
+});
+
+export const patchTaskParamsSchema = z.object({
+    id: z.coerce.number().int().positive("ID must be a positive integer"),
+});
+
 export const deleteTaskParamsSchema = z.object({
     id: z.coerce.number().int().positive("ID must be a positive integer"),
 });
