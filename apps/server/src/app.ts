382ba1883f3ab88e02149dd1ec540e8718308c09
kanicagoddard.kanica <kanicagoddard@gmail.com>
2025-07-22 01:06:19 +0100

    add cors to server

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import { tasksRouter } from './modules/tasks/task.routes';
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
+const allowedOrigins = [
+    "https://tasks-ui.herokuapp.com",
+    "http://localhost:5173", // for local dev
+];
+
+app.use(cors({
+    origin: allowedOrigins
+}));
 
 // Apply middlewares
 app.use(express.json())
