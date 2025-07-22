eacfa79d111ec480180b521fc14d22c9c7173205
kanicagoddard.kanica <kanicagoddard@gmail.com>
2025-07-22 01:06:45 +0100

    add API functions

---
+++ apps/client/src/api/tasks.ts
@@ -0,0 +1,56 @@
+export type Task = {
+    id: number;
+    title: string;
+    done: boolean;
+    createdAt: string;
+};
+
+export async function fetchTasks(): Promise<Task[]> {
+    const res = await fetch("/api/v1/tasks");
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch tasks");
+    }
+
+    return res.json().then((res) => res.data);
+}
+
+export async function createTask(title: string): Promise<Task> {
+    const res = await fetch("/api/v1/tasks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title }),
+    });
+
+    if (!res.ok) {
+        const errData = await res.json();
+        throw new Error(errData.message || "Failed to create task");
+    }
+
+    const json = await res.json();
+    return json.data[0];
+}
+
+export async function updateTaskDone(id: number, done: boolean): Promise<void> {
+    const res = await fetch(`/api/v1/tasks/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ done }),
+    });
+
+    if (!res.ok) {
+        throw new Error("Failed to update task");
+    }
+}
+
+export async function deleteTask(id: number): Promise<void> {
+    const res = await fetch(`/api/v1/tasks/${id}`, {
+        method: "DELETE",
+    });
+
+    if (!res.ok) {
+        throw new Error("Failed to delete task");
+    }
+}
